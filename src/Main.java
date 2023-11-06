09456ba5e8705613ab48552048366086a87db094
Waylon <weese.619547@swl.k12.oh.us>
2023-11-06 09:08:11 -0500

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Loop 1");
+        for(int a = 0; a <= 30; a++){
+            System.out.println(a);
+        }
+        System.out.println("Loop 2");
+        for(int a = 30; a >= 0; a--){
+            System.out.println(a);
+        }
+        System.out.println("Loop 3");
+        for(int a = 0; a <= 18; a += 3){
+            System.out.println(a);
+        }
+        System.out.println("Loop 4");
+        for(int a = 10; a >= 0; a -= 2){
+            System.out.println(a);
+        }
     }
 }
